1e43c60a705cec7509824df541b06c29fbe62df1
Sithum Vihanga <73056402+sithumvflash@users.noreply.github.com>
2023-05-29 23:36:53 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import classes from './App.module.css';
+import ProductData from './ProductData';
 
 function App() {
   return (
@@ -14,6 +15,20 @@ function App() {
       <div className={classes.MainContainer}>
         <div className={classes.ProductPreview}>
           <img src="https://imgur.com/iOeUBV7.png" alt="Product Preview" />
+        </div>
+        {/* find a way to upload node modules to github */}
+        <div className={classes.ProductData}>
+          <h1 className={classes.ProductTitle}>{ProductData.title}</h1>
+          <p className={classes.ProductDescription}>{ProductData.description}</p>
+
+          <h3 className={classes.SectionHeading}>Select Color</h3>
+          <div>
+            <img className={classes.ProductImage} src="https://imgur.com/iOeUBV7.png" alt="Black Colour Watch" />
+            <img className={classes.ProductImage} src="https://imgur.com/PTgQlim.png" alt="Red Colour Watch" />
+            <img className={classes.ProductImage} src="https://imgur.com/Mplj1YR.png" alt="Blue Colour Watch" />
+            <img className={classes.ProductImage} src="https://imgur.com/xSIK4M8.png" alt="Purple Colour Watch" />
+
+          </div>
         </div>
 
       </div>
