23a6e23da1bcba9cd13ba73b1eb261e58d6009cf
Sithum Vihanga <73056402+sithumvflash@users.noreply.github.com>
2023-06-02 23:21:22 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -23,14 +23,23 @@ function App() {
 
           <h3 className={classes.SectionHeading}>Select Color</h3>
           <div>
-            <img className={classes.ProductImage} src="https://imgur.com/iOeUBV7.png" alt="Black Colour Watch" />
+            {/* <img className={[classes.ProductImage, classes.SelectedProductImage].join('')} src="https://imgur.com/iOeUBV7.png" alt="Black Colour Watch" /> */}
+            <img className={classes.ProductImg} src="https://imgur.com/iOeUBV7.png" alt="Black Colour Watch" />
             <img className={classes.ProductImage} src="https://imgur.com/PTgQlim.png" alt="Red Colour Watch" />
             <img className={classes.ProductImage} src="https://imgur.com/Mplj1YR.png" alt="Blue Colour Watch" />
             <img className={classes.ProductImage} src="https://imgur.com/xSIK4M8.png" alt="Purple Colour Watch" />
 
           </div>
-        </div>
+          <div>
+            <h3 className={classes.SectionHeading}>Features</h3>
+          </div>
 
+          <div>
+            <button className={classes.FeatureItem}>Time</button>
+            <button className={classes.FeatureItem}>Heart Rate</button>
+          </div>
+
+        </div>
       </div>
     </div>
   );
