d87891b21a3429d91c4c577fcd4d584a1964c3a0
Sithum Vihanga <73056402+sithumvflash@users.noreply.github.com>
2023-06-05 00:54:06 +0530

    Update ProductPreview.js

--- src/ProductPreview.js
+++ src/ProductPreview.js
@@ -1,1 +1,19 @@
+import React from "react";
 
+const ProductPreview = (props) => {
+  return (
+    <div>
+      <img src="https://imgur.com/iOeUBV7.png" alt="Product Preview" />
+
+      {/* <div className={classes.FeatureData}>
+  <p>{`${currentHour}:${currentMinute}`}</p>
+</div> */}
+      <div className={classes.HeartBeatSection}>
+        <i class="fa-solid fa-heart-pulse"></i>
+        <p>78</p>
+      </div>
+    </div>
+  );
+}
+
+export default ProductPreview;
