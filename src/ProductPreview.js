6e71e99f864f14a6995dfd6829dd4f501086f207
Sithum Vihanga <73056402+sithumvflash@users.noreply.github.com>
2023-06-06 14:03:47 +0530

    Update ProductPreview.js

--- src/ProductPreview.js
+++ src/ProductPreview.js
@@ -1,17 +1,21 @@
 import React from "react";
+import classes from './ProductPreview.module.css';
 
 const ProductPreview = (props) => {
+  const currentHour = new Date().getHours() > 9 ? new Date().getHours() : '0' + new Date().getHours();
+  const currentMinute = new Date().getMinutes() > 9 ? new Date().getMinutes() : '0' + new Date().getMinutes();
   return (
-    <div>
+    <div className={classes.ProductPreview}>
       <img src="https://imgur.com/iOeUBV7.png" alt="Product Preview" />
 
-      {/* <div className={classes.FeatureData}>
-  <p>{`${currentHour}:${currentMinute}`}</p>
-</div> */}
-      <div className={classes.HeartBeatSection}>
-        <i class="fa-solid fa-heart-pulse"></i>
-        <p>78</p>
+      <div className={classes.FeatureData}>
+        <p>{`${currentHour}:${currentMinute}`}</p>
       </div>
+
+      {/* <div className={classes.HeartBeatSection}>
+        <i className="fa-solid fa-heart-pulse"></i>
+        <p>78</p>
+      </div> */}
     </div>
   );
 }
